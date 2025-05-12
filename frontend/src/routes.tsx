457204cc760499fdb3d3b78bb5f680580d04bb41
harleit <leitteharley@gmail.com>
2025-05-12 09:51:19 -0400

    fix: removido rota de novo usuário do layout padrão

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,19 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
-import { Layout } from './App';
-import { HomePage } from './pages/homePage';
-import { AboutPage } from './pages/aboutPage';
-import { NewProject } from './pages/Project/New';
-import { NewUser } from './pages/User/New';
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./App";
+import { AboutPage } from "./pages/aboutPage";
+import { NewProject } from "./pages/Project/New";
+import { NewUser } from "./pages/User/New";
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} /> {/* Página inicial */}
+      <Route element={<Layout />}>
         <Route path="about" element={<AboutPage />} />
-        <Route path="project/new" element={<NewProject/>}/>
-        <Route path = "user/new" element = {<NewUser/>}/>
+        <Route path="project/new" element={<NewProject />} />
       </Route>
+
+      <Route index element={<NewUser />} />
     </Routes>
   );
 }
