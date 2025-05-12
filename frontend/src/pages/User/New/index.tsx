fa7f6bff58dcfd377a08af0e5ba4977e24bd58db
harleit <leitteharley@gmail.com>
2025-05-12 09:52:01 -0400

    fix: restilizado página de novo usuário para funcionar como página inicial do projeto

--- frontend/src/pages/User/New/index.tsx
+++ frontend/src/pages/User/New/index.tsx
@@ -5,124 +5,156 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createUser } from "@/services/user.service";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
+import FormImage from "../../../assets/form-image.png";
+import { useNavigate } from "react-router-dom";
+import { KeyRound, Mail, User } from "lucide-react";
 
 const schema = z.object({
-    userName: z.string().min(10, {message: "Nome obrigatório"}),
-    userEmail:  z.string().min(20, {message: "Email obrigatório"}),
-    userPassword: z.string().min(5, {message: "Senha obrigatória"})
-}); 
+  userName: z
+    .string()
+    .min(3, { message: "O campo deve ter pelo menos 3 caracteres." })
+    .max(50, { message: "O campo deve ter no máximo 50 caracteres" }),
+  userEmail: z.string().email({ message: "Digite um e-mail válido." }),
+  userPassword: z.string().min(1, { message: "Senha obrigatória" }),
+});
 
-export function NewUser(){
-    const { 
-        register, 
-        handleSubmit, 
-        formState: {errors, isValid},
-        reset,
-    } = useForm<z.infer<typeof schema>>({
-        mode: "onChange",
-        resolver: zodResolver(schema)
-    }); 
+export function NewUser() {
+  let navigate = useNavigate();
 
-    const onSubmit = async(data: z.infer<typeof schema>) => {
-        try{
-            const payload = {
-                nomeCompleto: data.userName,
-                email: data.userEmail,
-                senha: data.userPassword
-            };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<z.infer<typeof schema>>({
+    mode: "onChange",
+    resolver: zodResolver(schema),
+  });
 
-            await createUser(payload); 
+  const onSubmit = async (data: z.infer<typeof schema>) => {
+    try {
+      const payload = {
+        nomeCompleto: data.userName,
+        email: data.userEmail,
+        senha: data.userPassword,
+      };
 
-            /* Sucesso */
-            toast("Usuário criado!",{
-                description: "O usuário foi criado com sucesso!",
-            });
-            reset(); 
-        } catch(error){
-            //Erro no envio
-            toast.error("Erro ao criar usuário", {
-                description: "Verifique os dados e tente novamente.",
-            });
-            console.error("Erro:", error);
-        }
+      await createUser(payload);
+
+      /* Sucesso */
+      toast("Usuário criado!", {
+        description: "O usuário foi criado com sucesso!",
+      });
+      reset();
+      navigate("project/new");
+    } catch (error) {
+      //Erro no envio
+      toast.error("Erro ao criar usuário", {
+        description: "Verifique os dados e tente novamente.",
+      });
+      console.error("Erro:", error);
     }
+  };
 
-    return(
-        <div className = "flex justify-center w-1/2 p-[10px] bg-gray-50 rounded-lg shadow-lg">
-            <form 
-            onSubmit = {handleSubmit(onSubmit)}
-            className = "flex flex-col gap-8 w-full">
-                {/* nome completo */}
-                <div>
-                    <label htmlFor="userName"
-                    className = "block mb-2">
-                        Nome completo
-                    </label>
-                    <Input
-                    {...register("userName")}
-                    placeholder = "Digite seu nome completo"
-                    className = "w-full"
-                    />
-                    {errors.userName? (
-                        <p className = "text-red-500">{errors.userName.message}</p>
-                    ) : (
-                        <p className = "text-sm text-gray-500">
-                            Esse é o seu nome completo
-                        </p>
-                    )}
+  return (
+    <div className="flex justify-center items-center h-dvh">
+      <div className="flex justify-center w-1/2 p-[10px] bg-gray-50 rounded-lg shadow-lg gap-6">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-8 w-1/2"
+        >
+          {/* nome completo */}
+          <div>
+            <div className="flex items-center gap-2">
+              <User size={18} className="text-blue-500" />
+              <label htmlFor="userName" className="block mb-2">
+                Nome completo
+              </label>
+            </div>
+            <Input
+              {...register("userName")}
+              placeholder="Digite seu nome completo"
+              className="w-full"
+            />
+            {errors.userName ? (
+              <p className="text-sm text-red-500">{errors.userName.message}</p>
+            ) : (
+              <p className="text-sm text-blue-500">
+                Esse é o seu nome completo
+              </p>
+            )}
+          </div>
 
-                </div>
+          {/* email  */}
+          <div>
+            <div className="flex items-center gap-2">
+              <Mail size={18} className="text-blue-500" />
+              <label htmlFor="userEmail" className="block mb-2">
+                Email
+              </label>
+            </div>
+            <Input
+              type="email"
+              {...register("userEmail")}
+              placeholder="Digite seu Email"
+              className="w-full"
+            />
+            {errors.userEmail ? (
+              <p className="text-sm text-red-500">{errors.userEmail.message}</p>
+            ) : (
+              <p className="text-sm text-blue-500">
+                Esse será o seu Email cadastrado
+              </p>
+            )}
+          </div>
 
-                {/* email  */}
-                <div>
-                    <label htmlFor="userEmail"
-                    className = "block mb-2">
-                        Email
-                    </label>
-                    <Input 
-                    type = "email"
-                    {...register("userEmail")}
-                    placeholder = "Digite seu Email"
-                    className = "w-full"
-                    />
-                    {errors.userEmail ? (
-                        <p className = "text-red-500">{errors.userEmail.message}</p>
-                    ) : (
-                        <p className = "text-sm text-gray-500">
-                            Esse será o seu Email cadastrado
-                        </p>
-                    )}
-                    
-                </div>
+          {/* senha */}
+          <div>
+            <div className="flex items-center gap-2">
+              <KeyRound size={18} className="text-blue-500" />
+              <label htmlFor="userPassword" className="block mb-2">
+                Senha
+              </label>
+            </div>
+            <Input
+              type="password"
+              {...register("userPassword")}
+              placeholder="Digite sua senha"
+              className="w-full"
+            />
+            {errors.userPassword ? (
+              <p className="text-sm text-red-500">
+                {errors.userPassword.message}
+              </p>
+            ) : (
+              <p className="text-sm text-blue-500">Sua senha</p>
+            )}
+          </div>
 
-                {/* senha */}
-                <div>
-                    <label htmlFor="userPassword"
-                    className = "block mb-2">
-                        Senha
-                    </label>
-                    <Input
-                    type = "password"
-                    {...register("userPassword")}
-                    placeholder = "Digite sua senha"
-                    className = "w-full"
-                    />
-                    {errors.userPassword ? (
-                        <p className = "text-red-500">{errors.userPassword.message}</p>
-                    ) : (
-                        <p className = "text-sm text-gray-500">
-                            Sua senha 
-                        </p>
-                    )}
-
-                </div>
-
-                {/* Botão de submit */}
-                <Button className = "cursor-pointer"
-                type = "submit">Cadastrar</Button>
-
-            </form>
-            
+          {/* Botão de submit */}
+          <Button className="cursor-pointer bg-blue-500" type="submit">
+            Cadastrar
+          </Button>
+        </form>
+        <div className="flex flex-col w-1/2 justify-around">
+          <div className="flex w-full justify-between">
+            Já possui cadastro?
+            <Button
+              className="bg-blue-500 cursor-pointer"
+              onClick={() => navigate("project/new")}
+            >
+              Entrar
+            </Button>
+          </div>
+          <div>
+            <img
+              src={FormImage}
+              alt="Imagem de duas pessoas dando mãos"
+              className="shadow-lg"
+            />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
