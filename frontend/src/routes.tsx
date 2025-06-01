ad8e459e93dca9726a152af29490fa99c4269615
harleit <leitteharley@gmail.com>
2025-06-01 18:17:18 -0400

    feat: add routes for ListProjects and ListActivity pages in AppRoutes

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,13 +3,17 @@ import { Layout } from "./App";
 import { AboutPage } from "./pages/aboutPage";
 import { NewProject } from "./pages/Project/New";
 import { NewUser } from "./pages/User/New";
+import { ListActivity } from "./pages/ActiviyHub/List";
+import { ListProjects } from "./pages/Project/List";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="about" element={<AboutPage />} />
+        <Route path="project" element={<ListProjects />} />
         <Route path="project/new" element={<NewProject />} />
+        <Route path="activity" element={<ListActivity />} />
       </Route>
 
       <Route index element={<NewUser />} />
