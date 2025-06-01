39380ce47f755e229859fc9e8c3e56d0e7287d4d
harleit <leitteharley@gmail.com>
2025-06-01 18:17:08 -0400

    feat: integrate React Query by adding QueryClientProvider in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { AppRoutes } from './routes'
-import { Toaster } from 'sonner'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { AppRoutes } from "./routes";
+import { Toaster } from "sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-      <AppRoutes/>
-      <Toaster/>
-    </Router>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <AppRoutes />
+        <Toaster />
+      </Router>
+    </QueryClientProvider>
+  </StrictMode>
+);
